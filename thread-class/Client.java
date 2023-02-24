4bbb114dbf00f93a8b5d96865560538affb23bb5
John Roberts <roberts.john@gmail.com>
2023-02-23 19:08:58 -0800

    reverted

--- thread-class/Client.java
+++ thread-class/Client.java
@@ -1,14 +1,9 @@
 public class Client {
-  public static void main( String[] args ) {
-    LetterThread a = new LetterThread( 'a' );
-    LetterThread b = new LetterThread( 'b' );
+  public static void main(String[] args) {
+    LetterThread a = new LetterThread('a');
+    LetterThread b = new LetterThread('b');
     CounterThread c = new CounterThread();
 
-    a.setPriority(Thread.MAX_PRIORITY);
-    b.setPriority(5);
-    c.setPriority(Thread.MIN_PRIORITY);
-
-    System.out.println(String.format("Min: %d, Max: %d", Thread.MIN_PRIORITY, Thread.MAX_PRIORITY));
     a.start();
     b.start();
     c.start();
